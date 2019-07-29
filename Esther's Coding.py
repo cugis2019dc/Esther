427bbfa3e0a9cf919109a66d064702aaa3809dbd
Esther23245 <53440583+Esther23245@users.noreply.github.com>
2019-07-29 13:56:15 -0400

    Add files via upload

--- Esther's Coding.py
+++ Esther's Coding.py
@@ -9,6 +9,21 @@ Hello my name is Esther
 print("my name is Esther")
 5/2
 print("5*2")
-print("5-2")
+print(5-2)
 print("5^2")
 print("8/9*3")
+
+def multiply(a,b)
+        multiply= a*b
+        print(multiply)
+        def sum (3,8,4)
+sum = 3+8+4
+
+def area(a,b,c)
+Area= (a,b,c)
+print(Area) 
+area=(0,3,4,6)
+
+def triArea(b,h):
+    triArea=5*b*h
+    print('the ara of the triangle with the base of',b,'and a heiht of'h,is' triArea )    
