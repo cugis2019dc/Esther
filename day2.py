9c5ae4f35acef5102abb6272b27e449821029655
Esther23245 <53440583+Esther23245@users.noreply.github.com>
2019-07-31 10:10:00 -0400

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,69 @@
+"""
+
+
+def candbutry (a):
+   result=a+10 
+   print (result)
+   
+   
+number=23  
+#cadbury1:
+print(number)
+
+cadburyType1= "Milk Chocolate"
+cadburyType2 = "Dark Chocolate"
+cadburyType3 = "White Chocolate"
+
+#print("There are"cardburyType1,",",cardburyType2,",and",cardburyType3,"in the box.")
+
+print("Hello Esther")
+
+a="Esther"
+
+print("Hello",a)
+ 
+
+name= input ("please enter age")
+            
+print("Your name is",name)
+
+age=input("please enter your age")
+
+print("Thank you. You entered",age)
+
+ageint=int(age)
+ageint
+"""
+import math
+
+dir(math)
+
+math.factorial(0)
+
+math.pow(6,15)
+
+math.pow(8,0.33) 
+
+def CubicRoot(a):
+    print("The cubic root of",a,"is",math.pow(a,(1/3)))
+
+CubicRoot(8)
+
+n=int(3)
+d=int(9)
+x=1/n
+print()
+
+def cubic(x):
+    cubic= math.pow(x,1/3)
+    print(cubic)
+    
+cubic(4)    
+import math
+ a=int(input("enter a value))
+
+def cadbury(a):
+    cadbury=math.pow(a,1/3)
+    print("the cubic root of",a, "is",cardbury)
+    
+    cadbury(a)
